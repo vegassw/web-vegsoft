49e586b2811a398dc1ce239a8a9ead223076d26e
Douglas Vegas <vegasdjg@gmail.com>
2024-11-30 01:06:35 -0300

    Primer commit

---
+++ js/vegsoft.js
@@ -0,0 +1,4 @@
+// Animaciones con GSAP
+gsap.from("#hero h1", { duration: 1.5, y: -50, opacity: 0 });
+gsap.from("#hero p", { duration: 1.5, delay: 0.5, y: 50, opacity: 0 });
+gsap.from(".btn", { duration: 1.5, delay: 1, scale: 0.8, opacity: 0 });
